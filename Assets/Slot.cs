28eab2df328ed276414e62ac71bec15ade3f5757
Nishant Khalate <nishantkhalate10@gmail.com>
2020-10-15 19:15:50 +0530

    Inventory_step1

---
+++ Assets/Slot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slot : MonoBehaviour
+{
+    public Inventory inventory;
+    public int slotID;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (transform.childCount <= 0)
+        {
+            inventory.isFull[slotID] = false;
+        }
+    }
+
+    public void DropItem()
+    {
+        foreach (Transform child in transform)
+        {
+            child.GetComponent<ScaresManager>().SpawnDroppedItem();
+            GameObject.Destroy(child.gameObject);
+        }
+    }
+}
